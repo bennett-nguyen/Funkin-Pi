74e3aa6d36c400d021db04693183b32966d3492a
bennett-nguyen <7akk4r2821@gmail.com>
2022-01-09 14:49:51 +0700

    finally get the control to work

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.constants import K_UP
 from game_loader import DisplaySurf
 from ui_componets import transparent_rect, transparent_rect2, objects 
 from sys import exit
@@ -8,46 +9,53 @@ drawn = []
 pygame.init()
 
 
-def draw_window():    
+def draw_window():
     DisplaySurf.Screen.fill('Black')
-    
+
     DisplaySurf.Screen.blit(transparent_rect.surface, transparent_rect.rect)
     DisplaySurf.Screen.blit(transparent_rect.surface, transparent_rect2.rect)
     transparent_rect.draw_arrow()
     transparent_rect2.draw_arrow()
+ 
     pygame.draw.line(DisplaySurf.Screen, "White", (DisplaySurf.WIDTH/2,
                      0), (DisplaySurf.WIDTH/2, DisplaySurf.HEIGHT), 3)
 
     for i in objects:
         DisplaySurf.Screen.blit(i.surface, i.rect)
-        i.move()
         i.draw_arrow()
+        
+        i.move()
         i.move_arrow()
 
         if i.rect.y <= - i.surface.get_height():
             i.rect.y = DisplaySurf.HEIGHT + i.surface.get_height()
-            i.arrow_rect.y = i.rect.y - 20
+            i.arrow_rect.y = i.rect.y
 
     pygame.display.update()
 
 
 
 while True:
-    key = pygame.key.get_pressed()
-
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        
+ 
         if event.type == pygame.KEYDOWN:
-            for i in objects[:]:
-                if i.rect.colliderect(transparent_rect.rect): # and event.key == key[K_SPACE]:
-                    print("deleted")
-                    objects.remove(i)
+            for object in objects[:]:
+                if object.rect.colliderect(transparent_rect.rect):
+                    match event.key:
+                        case pygame.K_UP:
+                            objects.remove(object) if object.key == "up" else None
 
+                        case pygame.K_DOWN:
+                            objects.remove(object) if object.key == "down" else None
 
+                        case pygame.K_LEFT:
+                            objects.remove(object) if object.key == 'left' else None
+
+                        case pygame.K_RIGHT:
+                            objects.remove(object) if object.key == 'right' else None
 
     draw_window()
     DisplaySurf.Clock.tick(DisplaySurf.FPS)
--- ui_componets.py
+++ ui_componets.py
@@ -10,6 +10,7 @@ class TransparentSurf:
         # , pygame.SRCALPHA, 32)
 
         self.surface = pygame.Surface([game_loader.DisplaySurf.WIDTH/2/1.3, 100], pygame.SRCALPHA, 32)
+        # self.surface.fill('Red')
         self.rect = self.surface.get_rect(center=(x, y))
         
         self.left_arrow = image_loader.LEFT_ARROW
@@ -18,15 +19,15 @@ class TransparentSurf:
         self.right_arrow = image_loader.RIGHT_ARROW
 
         self.left_arrow_rect = self.left_arrow.get_rect(midleft = self.rect.midleft)
-        self.up_arrow_rect = self.up_arrow.get_rect(midleft = (self.rect.midleft[0] + 100, self.rect.midleft[1]))
-        self.down_arrow_rect = self.down_arrow.get_rect(midright = (self.rect.midright[0] - 100, self.rect.midleft[1]))
+        self.down_arrow_rect = self.down_arrow.get_rect(midleft = (self.rect.midleft[0] + 100, self.rect.midleft[1]))
+        self.up_arrow_rect = self.up_arrow.get_rect(midright = (self.rect.midright[0] - 100, self.rect.midright[1]))
         self.right_arrow_rect = self.right_arrow.get_rect(midright = self.rect.midright)
-    def draw_arrow(self):
 
-        game_loader.DisplaySurf.Screen.blit(self.left_arrow, self.left_arrow_rect)
-        game_loader.DisplaySurf.Screen.blit(self.up_arrow, self.up_arrow_rect)
-        game_loader.DisplaySurf.Screen.blit(self.down_arrow, self.down_arrow_rect)
-        game_loader.DisplaySurf.Screen.blit(self.right_arrow, self.right_arrow_rect)
+    def draw_arrow(self):
+            game_loader.DisplaySurf.Screen.blit(self.left_arrow, self.left_arrow_rect)
+            game_loader.DisplaySurf.Screen.blit(self.up_arrow, self.up_arrow_rect)
+            game_loader.DisplaySurf.Screen.blit(self.down_arrow, self.down_arrow_rect)
+            game_loader.DisplaySurf.Screen.blit(self.right_arrow, self.right_arrow_rect)
 
 class FlyingSurf:
     def __init__(self, y, arrow):
@@ -34,6 +35,7 @@ class FlyingSurf:
         self.y = y
 
         self.surface = pygame.Surface([game_loader.DisplaySurf.WIDTH/2/1.3, 100], pygame.SRCALPHA, 32)
+        # self.surface.fill('Red')
         self.rect = self.surface.get_rect(center = (self.x, self.y))
 
         self.arrow = arrow 
@@ -42,12 +44,13 @@ class FlyingSurf:
             case game_loader.Image.LEFT_ARROW:
                 self.arrow_rect = self.arrow.get_rect(midleft = (self.rect.midleft[0], self.y))
                 self.key = 'left'
-            case game_loader.Image.UP_ARROW:
-                self.arrow_rect = self.arrow.get_rect(midleft =  (self.rect.midleft[0] + 100, self.y))
-                self.key = 'up'
             case game_loader.Image.DOWN_ARROW:
-                self.arrow_rect = self.arrow.get_rect(midright =  (self.rect.midright[0] - 100, self.y))
+                self.arrow_rect = self.arrow.get_rect(midleft =  (self.rect.midleft[0] + 100, self.y))
                 self.key = 'down'
+
+            case game_loader.Image.UP_ARROW:
+                self.arrow_rect = self.arrow.get_rect(midright =  (self.rect.midright[0] - 100, self.y))
+                self.key = 'up'
             case game_loader.Image.RIGHT_ARROW:
                 self.arrow_rect = self.arrow.get_rect(midright =  (self.rect.midright[0], self.y))    
                 self.key = 'right'
@@ -68,8 +71,8 @@ transparent_rect = TransparentSurf(game_loader.DisplaySurf.WIDTH/2/2, 80)
 transparent_rect2 = TransparentSurf((game_loader.DisplaySurf.WIDTH/2/2)*3, 80)
 
 flying_object = FlyingSurf(875, game_loader.Image.LEFT_ARROW)
-flying_object_2 = FlyingSurf(1000, game_loader.Image.UP_ARROW)
-flying_object_3 = FlyingSurf(1125, game_loader.Image.DOWN_ARROW)
+flying_object_3 = FlyingSurf(1000, game_loader.Image.DOWN_ARROW)
+flying_object_2 = FlyingSurf(1125, game_loader.Image.UP_ARROW)
 flying_object_4 = FlyingSurf(1250, game_loader.Image.RIGHT_ARROW)
 
-objects = [flying_object, flying_object_2, flying_object_3, flying_object_4]
+objects = [flying_object, flying_object_3, flying_object_2, flying_object_4]
