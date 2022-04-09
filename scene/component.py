0f93d1ab56885d65147f71c4aa6cee90b504e0b8
Bennett <7akk4r2821@gmail.com>
2022-04-09 18:39:14 +0700

    👷 Introduced track selection [MOVING TEXT][PROGRESS: 1/3]

--- scene/component.py
+++ scene/component.py
@@ -3,22 +3,23 @@ import load.game_loader as game_loader
 
 pygame.init()
 
+
 class Scene:
     def __init__(self):
         self.redirect = None
         self.redirect_delay = 0
-        
+
         self.fade_delay = 0
-    
+
     def input(self):
         pass
-    
+
     def redraw(self):
         pass
-    
+
     def reset_attr(self):
         self.redirect = None
-    
+
     def set_direct(self):
         pass
 
@@ -29,37 +30,38 @@ class SceneSwitcher:
         self.current = self.scenes["start screen"]
         self.is_transitioning = False
         self.redirect_delay = 0
-        
+
         self.current_time = 0
         self.redirected_time = 0
-        
-        self.surface = pygame.Surface(game_loader.DisplaySurf.Screen.get_size())
-        self.surface.fill((0, 0, 0))
-        
+
+        self.screen = pygame.Surface(
+            game_loader.DisplaySurf.Screen.get_size())
+        self.screen.fill((0, 0, 0))
+
         self.alpha = 0
         self.fade_delay = 0
         self.fade_state = "OUT"
-        
+
     def change_scene(self):
         state = self.current.redirect
         self.current.reset_attr()
         self.current = self.scenes[state]
         self.reset_attr()
-    
+
     def fade(self):
         if self.fade_state == "OUT":
             self.alpha += 4
-            self.surface.set_alpha(self.alpha)
-            game_loader.DisplaySurf.Screen.blit(self.surface, (0, 0))
-            
+            self.screen.set_alpha(self.alpha)
+            game_loader.DisplaySurf.Screen.blit(self.screen, (0, 0))
+
             if self.alpha >= 255:
                 self.fade_state = "IN"
 
         elif self.fade_state == "IN":
             self.alpha -= 4
-            self.surface.set_alpha(self.alpha)
-            game_loader.DisplaySurf.Screen.blit(self.surface, (0, 0))
-            
+            self.screen.set_alpha(self.alpha)
+            game_loader.DisplaySurf.Screen.blit(self.screen, (0, 0))
+
             if self.alpha <= 0:
                 self.fade_state = "OUT"
                 self.alpha = 0
@@ -91,3 +93,86 @@ class SceneSwitcher:
 
     def reset_attr(self):
         self.redirect_delay = self.redirected_time = 0
+
+
+class MenuLogic:
+    def __init__(self, tracks):
+        self.tracks = tracks
+        self.centery = self.tracks[0].display_name_rect.centery
+        
+        self.track_index = 0
+        self.current_track = self.tracks[self.track_index]
+
+        self.speed = 20
+        self.jumping_speed = 30
+        
+        self.unfocus_alpha = 100
+
+        self.is_transitioning = self.is_moving_up = self.is_moving_down = self.jumping = False
+
+    def move_track(self):
+        if self.is_transitioning:
+            for track in self.tracks:
+                if self.is_moving_up:
+                    track.display_name_rect.centery -= self.jumping_speed if self.jumping else self.speed
+                elif self.is_moving_down:
+                    track.display_name_rect.centery += self.jumping_speed if self.jumping else self.speed
+
+            if self.current_track.display_name_rect.centery == self.centery:
+                self.is_moving_down = self.is_moving_up = self.is_transitioning = self.jumping = False
+
+    def input(self):
+        key = pygame.key.get_pressed()
+
+        if not self.is_transitioning:
+            if key[pygame.K_UP]:
+
+                self.is_transitioning = True
+
+                if self.track_index >= 1:
+                    self.is_moving_down = True
+                    self.track_index -= 1
+                else:
+                    self.is_moving_up = True
+                    self.jumping = True
+                    self.track_index = len(self.tracks) - 1
+
+                game_loader.Audio.SCROLL_MENU.play()
+
+            elif key[pygame.K_DOWN]:
+                
+                self.is_transitioning = True
+
+                if self.track_index + 1 < len(self.tracks):
+                    self.is_moving_up = True
+                    self.track_index += 1
+                else:
+                    self.is_moving_down = True
+                    self.jumping = True
+                    self.track_index = 0
+
+                game_loader.Audio.SCROLL_MENU.play()
+
+            self.current_track = self.tracks[self.track_index]
+
+    def redraw(self):
+        if self.is_transitioning:
+            self.move_track()
+
+        for track in self.tracks:
+            if track.display_name_rect.bottom < 250 or track.display_name_rect.top > game_loader.DisplaySurf.HEIGHT:
+                continue
+
+            if track == self.current_track:
+                track.display_name.set_alpha(255)
+            else:
+                track.display_name.set_alpha(self.unfocus_alpha)
+
+            
+            # only render text that is below the yellow rectangle or below the screen height
+            game_loader.DisplaySurf.Screen.blit(
+                track.display_name, track.display_name_rect)
+
+    def update(self):
+        self.input()
+        self.redraw()
--- scene/scenes.py
+++ scene/scenes.py
@@ -2,25 +2,26 @@ import pygame
 import load.game_loader as game_loader
 import general_component.component as genc
 from load.game_loader import Data
-from scene.component import Scene
+from scene.component import MenuLogic, Scene
 
 
 pygame.init()
+
 
 class StartScreen(Scene):
     def __init__(self):
         super().__init__()
         self.sound_1_effect_played = self.sound_2_effect_played = self.button_pressed = False
-        
+
         self.redirect = None
         self.redirect_delay = 3500
-        
+
         self.fade_delay = 2500
-        
+
         # custom fade in animation
         self.surf = pygame.Surface(game_loader.DisplaySurf.Screen.get_size())
         self.surf.fill((255, 255, 255))
-        
+
         self.alpha = 255
         # ----
 
@@ -32,7 +33,6 @@ class StartScreen(Scene):
             game_loader.Gallery.BUTTON_ACTIVATED_IMAGES, game_loader.DisplaySurf.WIDTH/2, game_loader.DisplaySurf.HEIGHT/2 + 270, 0.15)
         self.button_hit_box = genc.Surface(
             game_loader.DisplaySurf.WIDTH/2, game_loader.DisplaySurf.HEIGHT/2 + 270, 250, 120)
-        
 
     def redraw(self):
         game_loader.DisplaySurf.Screen.blit(game_loader.Gallery.LOGO, game_loader.Gallery.LOGO.get_rect(
@@ -52,7 +52,7 @@ class StartScreen(Scene):
             else:
                 self.deactivated_button.toggle_animation()
                 self.sound_2_effect_played = False
-                
+
         if self.alpha > -1:
             self.custom_fade_in()
 
@@ -66,18 +66,15 @@ class StartScreen(Scene):
         if self.button_pressed and not self.sound_1_effect_played:
             game_loader.Audio.CONFIRM_MENU.play()
             self.sound_1_effect_played = True
-            
+
     def custom_fade_in(self):
         self.alpha -= 3
         self.surf.set_alpha(self.alpha)
         game_loader.DisplaySurf.Screen.blit(self.surf, (0, 0))
-        
 
     def reset_attr(self):
         super().reset_attr()
         self.sound_1_effect_played = self.sound_2_effect_played = self.button_pressed = False
-
-
 
 
 class MenuScreen(Scene):
@@ -85,39 +82,54 @@ class MenuScreen(Scene):
         super().__init__()
         self.redirect = None
         self.redirect_delay = 1000
-        
+
         self.week_score = 0
+
+        self.yellow_rectangle = genc.Surface(
+            game_loader.DisplaySurf.WIDTH/2, 150, game_loader.DisplaySurf.WIDTH - 150, 200, (249, 209, 81))
         
-        self.top_rectangle = genc.Surface(game_loader.DisplaySurf.WIDTH/2, 150, game_loader.DisplaySurf.WIDTH - 150, 200, (249, 209, 81))
-        self.choose_your_track = game_loader.Font.TITLE_FONT_2.render("CHOOSE YOUR TRACK", True, "Black")
-        self.cyt_rect = self.choose_your_track.get_rect(center = self.top_rectangle.rect.center)
-        self.score_text = game_loader.Font.MENU_SCORE.render(f"SCORE: {self.week_score}", True, "White")
-        self.st_rect = self.score_text.get_rect(midleft = (self.top_rectangle.rect.midleft[0], 25))
+        self.choose_your_track = game_loader.Font.TITLE_FONT_2.render(
+            "CHOOSE YOUR TRACK", True, "Black")
+        self.cyt_rect = self.choose_your_track.get_rect(
+            center=self.yellow_rectangle.rect.center)
+        
+        self.score_text = game_loader.Font.MENU_SCORE.render(
+            f"SCORE: {self.week_score}", True, "White")
+        self.st_rect = self.score_text.get_rect(
+            midleft=(self.yellow_rectangle.rect.midleft[0], 25))
 
+        
+        self.track_chooser_rect = genc.Surface(
+            game_loader.DisplaySurf.WIDTH/2, 470, 270, 300)
+        self.pointer = genc.ImageAnimation(
+            game_loader.Gallery.POINTER, self.track_chooser_rect.rect.centerx + 250, self.track_chooser_rect.rect.centery, 0.1)
+        self.tracks = Data.descriptions
 
-        self.some_rectangle = genc.Surface(game_loader.DisplaySurf.WIDTH/2, 470, 270, 300)
-        self.pointer = genc.ImageAnimation(game_loader.Gallery.POINTER, self.some_rectangle.rect.centerx + 250, self.some_rectangle.rect.centery, 0.1)
-        self.tracks = Data.descriptions        
-    
-    def draw_tracks(self):
+        
         distance = 0
-    
-        for track in self.tracks:
-            track.init_rect_coordinates(self.some_rectangle.rect.centerx, self.some_rectangle.rect.centery + distance)
-            game_loader.DisplaySurf.Screen.blit(track.display_name, track.display_name_rect)
-            distance += 100
-    
-    def redraw(self):
-        game_loader.DisplaySurf.Screen.blit(self.top_rectangle.surface, self.top_rectangle.rect)
-        game_loader.DisplaySurf.Screen.blit(self.choose_your_track, self.cyt_rect)
-        game_loader.DisplaySurf.Screen.blit(self.score_text, self.st_rect)
-        
-        self.pointer.toggle_animation()
-        
-        self.draw_tracks()
-        
-    def input(self):
-        keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_ESCAPE]:
+        for track in self.tracks:
+            track.init_rect_coordinates(
+                self.track_chooser_rect.rect.centerx, self.track_chooser_rect.rect.centery + distance)
+            distance += 120
+
+        self.logic = MenuLogic(self.tracks)
+
+    def redraw(self):
+        self.logic.update()
+        
+        game_loader.DisplaySurf.Screen.blit(
+            self.yellow_rectangle.surface, self.yellow_rectangle.rect)
+        
+
+        game_loader.DisplaySurf.Screen.blit(
+            self.choose_your_track, self.cyt_rect)
+        game_loader.DisplaySurf.Screen.blit(self.score_text, self.st_rect)
+
+        self.pointer.toggle_animation()
+
+    def input(self):
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_ESCAPE]:
             self.redirect = "start screen"
