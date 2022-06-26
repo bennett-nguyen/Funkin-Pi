40acd386e11fcc2c96346411b9475547e421f024
bennett-nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2022-06-26 08:22:39 +0700

    ✔ Implemented new pre-start-screen

--- start_screen.py
+++ start_screen.py
@@ -1,24 +1,28 @@
 import pygame
 import time
-from load.game_loader import DisplaySurf, Audio
+from secrets import choice
+from load.game_loader import DisplaySurf, Audio, Message
 from scene.component import SceneSwitcher
-from scene.scenes import StartScreen, MenuScreen, MainGame
+from scene.scenes import StartScreen, MenuScreen, MainGame, PreStartScreen
 from sys import exit
 
 pygame.init()
 
-
 game_scenes = {
+    "pre start screen 1": PreStartScreen(Message._req_message_list_1, 700, 800, "pre start screen 2"),
+    "pre start screen 2": PreStartScreen(Message._req_message_list_2, 700, 800, "pre start screen 3"),
+    "pre start screen 3": PreStartScreen(Message._req_message_list_3, 700, 800, "pre start screen 4"),
+    "pre start screen 4": PreStartScreen(choice(Message._opt_message_list), 700, 1000, "start screen"),
     "start screen": StartScreen(),
     "menu screen": MenuScreen(),
     "main game": MainGame()
 }
 
-switcher = SceneSwitcher(game_scenes)
+switcher = SceneSwitcher(game_scenes, start="pre start screen 1")
 
 
 def game():
-    # Audio.FREAKY_MENU.play(-1)
+    Audio.FREAKY_MENU.play(-1)
     dt = 0 # delta time
     prev_time = time.time()
 
