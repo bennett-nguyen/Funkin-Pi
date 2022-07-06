7685b52a90508d6c765ccfcd51fd099b2d3b2f3d
bennett-nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2022-07-06 15:18:47 +0700

    Renamed to entry.py

---
+++ entry.py
@@ -0,0 +1,55 @@
+import time
+import pygame
+import source.load.ds as ds
+import source.load.assets as assets
+import source.load.constant as const
+
+from sys import exit
+from source.load.shared import shared_data
+from source.load.switcher import SceneSwitcher
+from source.comp.scenes.main_game import MainGame
+from source.comp.scenes.menu_screen import MenuScreen
+from source.comp.scenes.start_screen import StartScreen
+from source.comp.scenes.pre_start_screen import PreStartScreen
+
+pygame.init()
+
+game_scenes = {
+    "pre start screen 1": PreStartScreen(assets.Message.req_message_list_1, 700, 800, "pre start screen 2"),
+    "pre start screen 2": PreStartScreen(assets.Message.req_message_list_2, 700, 800, "pre start screen 3"),
+    "pre start screen 3": PreStartScreen(assets.Message.req_message_list_3, 700, 800, "pre start screen 4"),
+    "pre start screen 4": PreStartScreen(assets.Message.opt_message_list, 700, 1000, "start screen"),
+    "start screen": StartScreen(),
+    "menu screen": MenuScreen(),
+    "main game": MainGame()
+}
+
+switcher = SceneSwitcher(game_scenes, start="pre start screen 1")
+
+def game():
+    assets.Audio.FREAKY_MENU.play(-1)
+    dt = 0 # delta time
+    prev_time = time.time()
+
+    while True:
+        ds.screen.fill('Black')
+        ds.clock.tick(const.FPS)
+
+        now = time.time()
+        dt = float(now - prev_time)
+        prev_time = now
+
+        events = pygame.event.get()
+        shared_data.dt = dt
+        shared_data.events = events
+
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        switcher.update()
+        pygame.display.update()
+        
+if __name__ == "__main__":
+    game()
